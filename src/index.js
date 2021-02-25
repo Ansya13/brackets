205c7da05d5c4b308bb0faa24f658e08efc9f17e
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-02-25 16:47:17 +0200

    feat: add aswer for 11 task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < str.length && i < bracketsConfig.length; i++) {
+        if (!str.includes(bracketsConfig)) {
+            return true
+        }
+        else {
+            return true
+        }
+    }
 }
