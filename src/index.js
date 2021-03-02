cf9f02601f3cae20b00bfa0e7f511204b301114a
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-03-02 19:52:28 +0200

    feat: add "Brackets" solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-    for (let i = 0; i < str.length && i < bracketsConfig.length; i++) {
-        if (!str.includes(bracketsConfig)) {
-            return true
-        }
-        else {
-            return true
-        }
-    }
+    let arr = str.split('');
+   for (let i = 1; i < arr.length; i++) {
+       for (let char= 0; char < bracketsConfig.length; char++) {
+           if (arr[i] == bracketsConfig[char][1] && arr[i - 1] == bracketsConfig[char][0]) {
+               arr.splice(i - 1, 2);
+         i-=2;
+       }
+     }
+   }
+   if (arr.length > 0) return false;
+     else return true;
 }
+
+
+
+
